c2b53fc99635c840ac368e441eeb047cc3a5d3bc
Nabin Khatiwada <nawin@Nawins.local>
2022-06-29 11:12:41 +0545

    boiler plate code removed

--- src/main/java/com/example/demo/errors/RequestError.java
+++ src/main/java/com/example/demo/errors/RequestError.java
@@ -1,27 +1,13 @@
 package com.example.demo.errors;
 
+import lombok.*;
+
+@Data
+@EqualsAndHashCode
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class RequestError extends IllegalStateException {
     private int statusCode;
     private String statusMessage;
-
-    public RequestError(int code, String statusMessage) {
-        this.statusCode = code;
-        this.statusMessage = statusMessage;
-    }
-
-    public int getStatusCode() {
-        return statusCode;
-    }
-
-    public void setStatusCode(int code) {
-        this.statusCode = code;
-    }
-
-    public String getStatusMessage() {
-        return statusMessage;
-    }
-
-    public void setStatusMessage(String message) {
-        this.statusMessage = message;
-    }
 }
